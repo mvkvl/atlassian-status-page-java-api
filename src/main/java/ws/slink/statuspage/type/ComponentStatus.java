08074dbd69e9fe0c8bc35a5fa7eff1255088eb56
Mikhail <kami@slink.ws>
2021-01-21 14:04:17 +1000

    added 'id' to ComponentStatus enum

--- src/main/java/ws/slink/statuspage/type/ComponentStatus.java
+++ src/main/java/ws/slink/statuspage/type/ComponentStatus.java
@@ -5,30 +5,35 @@ import com.google.gson.annotations.SerializedName;
 public enum ComponentStatus {
 
      @SerializedName("operational")
-     OPERATIONAL("operational"),
+     OPERATIONAL("operational", 1),
 
      @SerializedName("under_maintenance")
-     MAINTENANCE("under_maintenance"),
+     MAINTENANCE("under_maintenance", 2),
 
      @SerializedName("degraded_performance")
-     DEGRADED("degraded_performance"),
+     DEGRADED("degraded_performance", 3),
 
      @SerializedName("partial_outage")
-     PARTIAL_OUTAGE("partial_outage"),
+     PARTIAL_OUTAGE("partial_outage", 4),
 
      @SerializedName("major_outage")
-     MAJOR_OUTAGE("major_outage"),
+     MAJOR_OUTAGE("major_outage", 5),
 
      @SerializedName("")
-     NONE("")
+     NONE("", 0)
     ;
 
     private String value;
-    ComponentStatus(String value) {
+    private int id;
+    ComponentStatus(String value, int id) {
         this.value = value;
+        this.id = id;
     }
     public String value() {
         return this.value;
+    }
+    public int id() {
+        return this.id;
     }
     public static ComponentStatus of(String input){
         for(ComponentStatus v : values()){
